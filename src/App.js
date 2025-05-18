ea22b55fcd36259d257fddc0f508b45e18591e5b
Manjeet <jaaniraj283@gmail.com>
2025-05-18 14:13:02 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // 4 second loader
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer); // cleanup
+  }, []);
+
   return (
- <>
-<Header/>
-<Home/>
-<About/>
- </>
+    <>
+      {loading ? (
+        <div className="loader-wrapper">
+          <span className="loader"></span>
+        </div>
+      ) : (
+        <>
+          <Header />
+          <Home />
+          <About />
+        </>
+      )}
+    </>
   );
 }
 
